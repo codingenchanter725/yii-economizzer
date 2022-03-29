ed0315cf78a80d0f202bc9e1eda42032d250cf9f
codingenchanter725 <creativemoderndev0831@gmail.com>
2022-03-29 16:56:22 -0400

    Adding order addresses and items in the order view

--- backend/views/order/view.php
+++ backend/views/order/view.php
@@ -1,22 +1,25 @@
 <?php
 
+use common\models\Order;
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Order */
 
-$this->title = $model->id;
+$this->title = 'Details - Order #' . $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Orders', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
+
+$orderAddress = $model->orderAddress;
+
 ?>
 <div class="order-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
@@ -30,16 +33,68 @@ $this->params['breadcrumbs'][] = $this->title;
         'model' => $model,
         'attributes' => [
             'id',
-            'total_price',
-            'status',
+            'total_price:currency',
+            [
+                'attribute' => 'status',
+                'content' => function ($model) {
+                    if ($model->status === Order::STATUS_COMPLETED) {
+                        return Html::tag('span', 'Paid', ['class' => 'badge badge-success']);
+                    } else if ($model->status === Order::STATUS_DRAFT) {
+                        return Html::tag('span', 'Unpaid', ['class' => 'badge badge-secondary']);
+                    } else {
+                        return Html::tag('span', 'Failed', ['class' => 'badge badge-danger']);
+                    }
+                }
+
+            ],
             'firstname',
             'lastname',
             'email:email',
             'transaction_id',
             'paypal_order_id',
-            'created_at',
-            'created_by',
+            'created_at:datetime',
         ],
     ]) ?>
+    <h4>Address</h4>
+    <?= DetailView::widget([
+        'model' => $orderAddress,
+        'attributes' => [
+            'address',
+            'city',
+            'state',
+            'country',
+            'zipcode',
+        ],
+    ]) ?>
+
+    <h4>Order Items</h4>
+    <table class="table table-sm">
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <?php foreach ($model->orderItems as $item) : ?>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src="<?php echo $item->product->imageUrl() ?>" style="width: 100px">
+                        </td>
+                        <td>
+                            <?php echo $item->product_name ?>
+                        </td>
+                        <td>
+                            <?php echo $item->quantity ?>
+                        </td>
+                        <td>
+                            <?php echo Yii::$app->formatter->asCurrency($item->quantity * $item->unit_price) ?>
+                        </td>
+                    </tr>
+                </tbody>
+            <?php endforeach; ?>
+        </table>
 
 </div>
